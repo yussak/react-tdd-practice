97f2caa39a94f6a786f15653d3469c828afca9db
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-04-03 18:46:46 +0900

    失敗するテストを追加

--- src/__tests__/FizzBuzz.test.tsx
+++ src/__tests__/FizzBuzz.test.tsx
@@ -1,9 +1,22 @@
-// it.todo("blank")
+import { fireEvent, render } from "@testing-library/react"
 
 describe("Fixzz Buzz 問題の答えを表示する",()=>{
     // まず仕様を書く
     it.todo("入力欄に3の倍数を入力したら出力欄にFizzと表示される")
     it.todo("入力欄に5の倍数を入力したら出力欄にBuzzと表示される")
     it.todo("入力欄に3,5の両方の倍数を入力したら出力欄にFizzBuzzと表示される")
-    it.todo("それ以外の数字のときはそのまま出力欄に表示する")
+    
+    describe("それ以外の数字のときはそのまま出力欄に表示する",()=>{
+        it('1を入力したら出力欄に"1"と表示する',()=>{
+            // 準備
+            const {getByTestId} = render(<Index />)
+            const inputElement = getByTestId("input")
+            const outputElement = getByTestId("output")
+            // 実行
+            fireEvent.change(inputElement, {target:{value:"1"}})
+            
+            // 検証
+            expect(outputElement.textContent).toBe("1")
+        })
+    })
 })
